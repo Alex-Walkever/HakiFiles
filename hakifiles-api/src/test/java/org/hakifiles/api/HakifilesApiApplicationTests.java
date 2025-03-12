396ff6fc3d7c759a59b48ab5740bb41822d7931e
Walkever <alejandro.rodriguez.navarro92@gmail.com>
2025-03-11 21:40:48 -0600

    Start of Api app

---
+++ hakifiles-api/src/test/java/org/hakifiles/api/HakifilesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hakifiles.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakifilesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
